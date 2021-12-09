9c597a91352cce6611f741b68e7a8855cc25fee9
Jamie Dunbar <jpdunbar@iu.edu>
2021-12-08 22:01:37 -0500

    Adjusted the code to make it more readable

--- Dunbar Final Game/Assets/Scripts/PlayerControl.cs
+++ Dunbar Final Game/Assets/Scripts/PlayerControl.cs
@@ -8,51 +8,59 @@ using UnityEngine.SceneManagement;
 public class PlayerControl : MonoBehaviour
 {
     public Transform[] waypoints;
+
     public Vector3 movement;
     public Vector3 rotation;
+
     private Rigidbody rb;
+
     public GameObject Opponent;
     public GameObject Opponent2;
     public GameObject Opponent3;
     public GameObject Opponent4;
     public GameObject Opponent5;
+
     public TextMeshProUGUI currentPlace;
-    public float speed = 20f;
-    public float turnSpeed = 20f;
-    public int opponentCheckpoints;
-    public int opponent2Checkpoints;
-    public int opponent3Checkpoints;
-    public int opponent4Checkpoints;
-    public int opponent5Checkpoints;
-    public float opponentdistance;
-    public float opponent2distance;
-    public float opponent3distance;
-    public float opponent4distance;
-    public float opponent5distance;
-    public int playerCheckpoints;
-    public int playerPosition;
-    public float playerDistance;
-    public int index;
-    public int currentWaypoint;
-    public int reset;
-    public bool check;
-    public bool behind;
+
+    private float speed = 12f;
+    private float turnSpeed = 25f;
+
+    private int opponentCheckpoints;
+    private int opponent2Checkpoints;
+    private int opponent3Checkpoints;
+    private int opponent4Checkpoints;
+    private int opponent5Checkpoints;
+
+    private float opponentdistance;
+    private float opponent2distance;
+    private float opponent3distance;
+    private float opponent4distance;
+    private float opponent5distance;
+
+    private int playerCheckpoints;
+    private int playerPosition;
+    private float playerDistance;
+
+    private int index;
+    private int currentWaypoint;
+    private int reset;
+    private bool check;
+    private string[] checkpoints;
+    private int currentCheckpoint;
 
     private int[] allOpponentPositions = new int[5];
     private float[] allOpponentDistances = new float[5];
 
-    // Start is called before the first frame update
     void Start()
     {
+        checkpoints = new string[] { "Checkpoint", "Checkpoint2", "Checkpoint3", "Checkpoint4", "Checkpoint5", "Checkpoint6", "Checkpoint7", "Checkpoint8" };
         rb = GetComponent<Rigidbody>();
         playerCheckpoints = 0;
         reset = 7;
         currentWaypoint = 7;
         check = false;
-        behind = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (reset == waypoints.Length)
@@ -61,13 +69,16 @@ public class PlayerControl : MonoBehaviour
             currentWaypoint = 0;
         }
 
+        if (currentCheckpoint == 8)
+        {
+            currentCheckpoint = 0;
+        }
+
         if (check == false)
         {
             index = currentWaypoint;
             check = true;
         }
-
-        playerPosition = 1;
 
         opponentCheckpoints = Opponent.GetComponent<WaypointFollowing>().numberCheckpoints;
         opponent2Checkpoints = Opponent2.GetComponent<WaypointFollowing>().numberCheckpoints;
@@ -89,7 +100,7 @@ public class PlayerControl : MonoBehaviour
         transform.Rotate(rotation * Time.deltaTime * turnSpeed);
         movement.Normalize();
 
-        playerDistance = Mathf.Abs(transform.position.x - waypoints[index].position.x);
+        playerDistance = Vector3.Distance(waypoints[index].position, transform.position);
 
         allOpponentPositions[0] = opponentCheckpoints;
         allOpponentPositions[1] = opponent2Checkpoints;
@@ -108,33 +119,26 @@ public class PlayerControl : MonoBehaviour
 
     void determinePosition(int[] places, float[] distances)
     {
+        playerPosition = 1;
+
         for (int i = 0; i < places.Length; i++)
         {
-            //Debug.Log(playerCheckpoints + " " + places[i]);
             if (playerCheckpoints <= places[i])
             {
-                //playerPosition += 1;
-                behind = true;
-                //Debug.Log(playerDistance + " > " + distances[i] + " " + index);
-                if (playerDistance < distances[i] && behind == true)
+                if (playerDistance > distances[i])
                 {
-                    //playerPosition -= 1;
-                    //playerPosition += 1;
+                    playerPosition += 1;
                 }
             }
-            behind = false;
         }
         currentPlace.text = "Current Place: " + playerPosition.ToString();
     }
 
-    //Place
-    //0-0 So 6th?
-    //Then check if the player is behind any enemy
-
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Checkpoint"))
+        if (other.gameObject.CompareTag(checkpoints[currentCheckpoint]))
         {
+            currentCheckpoint += 1;
             playerCheckpoints += 1;
             reset += 1;
             currentWaypoint += 1;
--- Dunbar Final Game/Assets/Scripts/WaypointFollowing.cs
+++ Dunbar Final Game/Assets/Scripts/WaypointFollowing.cs
@@ -9,10 +9,8 @@ public class WaypointFollowing : MonoBehaviour
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
     public int currentWaypoint;
-    public WaypointFollowing script;
     int m_CurrentWaypointIndex;
     int reset;
-    int place;
     int ok;
     public int numberCheckpoints;
     public int mayber;
@@ -20,31 +18,24 @@ public class WaypointFollowing : MonoBehaviour
     bool check;
     bool start;
     public float distance;
-    public string[] checkpoints = new string[]{ "Checkpoint", "Checkpoint2", "Checkpoint3", "Checkpoint4", "Checkpoint5", "Checkpoint6", "Checkpoint7", "Checkpoint8",};
+    public string[] checkpoints;
     int currentCheckpoint;
-    //Thinking about getting a reference of each racer
-    //This will allow me to track score, position, maybe give them a new starting position?
-    //Distance is used for distance to next waypoint in case they are tied
-    //Will use greater than, not greater than equal to for next position in race
 
-    // Start is called before the first frame update
     void Start()
     {
+        checkpoints = new string[] { "Checkpoint", "Checkpoint2", "Checkpoint3", "Checkpoint4", "Checkpoint5", "Checkpoint6", "Checkpoint7", "Checkpoint8" };
         currentCheckpoint = 0;
-        checkpoints[0] = "Checkpoint";
         navMeshAgent.SetDestination(waypoints[0].position);
         currentWaypoint = 7;
         numberCheckpoints = 0;
         distance = 0;
         reset = 7;
-        place = 1;
         check = false;
         start = true;
         speed = Random.Range(10.0f, 12.0f);
         GetComponent<NavMeshAgent>().angularSpeed = 10;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (reset == waypoints.Length)
@@ -69,10 +60,8 @@ public class WaypointFollowing : MonoBehaviour
             GetComponent<NavMeshAgent>().angularSpeed = 120;
             GetComponent<NavMeshAgent>().speed = speed;
             navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
-            distance = Mathf.Abs(transform.position.x - waypoints[m_CurrentWaypointIndex].position.x);
+            distance = Vector3.Distance(waypoints[m_CurrentWaypointIndex].position, transform.position);
         }
-
-        Debug.Log(checkpoints[currentCheckpoint]);
     }
 
     private void OnTriggerExit(Collider other)
